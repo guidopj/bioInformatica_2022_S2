541ee36d9559f2146608503261142ac6dfe6fd2e
Guido Pujadas <guidopujadas@gmail.com>
2022-11-09 20:09:23 -0300

    alineamiento y BLAST

--- el_juego_de_la_vida.py
+++ el_juego_de_la_vida.py
@@ -1,79 +1,44 @@
 #Crea un script en Python que tome como argumento una secuencia proteica e imprima una cadena de 
 #ARN codificante. Podés usar de ejemplo el siguiente péptido (cadena corta de aminoácidos):
 
-#Sec1: 'ATVEKGGKHKTGPNEKGKKIFVQKCSQCHTVLHGLFGRKTGQA'
+Sec1 = 'ATVEKGGKHKTGPNEKGKKIFVQKCSQCHTVLHGLFGRKTGQA'
+
+import random
+
+#print(random.randrange(3, 9))
 
 genetic_code = {
-    'UUU': 'Phe',
-    'UUC': 'Phe',
-    'UUA': 'Leu',
-    'UUG': 'Leu',
-    'CUU': 'Leu',
-    'CUC': 'Leu',
-    'CUA': 'Leu',
-    'CUG': 'Leu',
-    'AUU': 'Iso',
-    'AUC': 'Iso',
-    'AUA': 'Iso',
-    'AUG': 'Met',
-    'GUU': 'Val',
-    'GUC': 'Val',
-    'GUA': 'Val',
-    'GUG': 'Val',
-    'UCU': 'Ser',
-    'UCC': 'Ser',
-    'UCA': 'Ser',
-    'UCG': 'Ser',
-    'CCU': 'Pro',
-    'CCC': 'Pro',
-    'CCA': 'Pro',
-    'CCG': 'Pro',
-    'ACU': 'Thr',
-    'ACC': 'Thr',
-    'ACA': 'Thr',
-    'ACG': 'Thr',
-    'GCU': 'Ala',
-    'GCC': 'Ala',
-    'GCA': 'Ala',
-    'GCG': 'Ala',
-    'UAU': 'Tyr',
-    'UAC': 'Tyr',
-    'UAA': 'STOP',
-    'UAG': 'STOP',
-    'CAU': 'His',
-    'CAC': 'His',
-    'CAU': 'His',
-    'CAA': 'Gln',
-    'CAG': 'Gln',
-    'AAU': 'Asn',
-    'AAC': 'Asn',
-    'AAA': 'Lys',
-    'AAG': 'Lys',
-    'GAU': 'Asp',
-    'GAC': 'Asp',
-    'GAA': 'Glu',
-    'GAG': 'Glu',
-    'UGU': 'Cys',
-    'UGC': 'Cys',
-    'UGA': 'STOP',
-    'UGG': 'Tyr',
-    'UGG': 'Tyr',
-    'CGU': 'Arg',
-    'CGC': 'Arg',
-    'CGA': 'Arg',
-    'CGG': 'Arg',
-    'AGU': 'Ser',
-    'AGC': 'Ser',
-    'AGA': 'Arg',
-    'AGG': 'Arg',
-    'GGU': 'Gly',
-    'GGC': 'Gly',
-    'GGA': 'Gly',
-    'GGG': 'Gly',
+    'V': ['GUU', 'GUC', 'GUA', 'GUG'], #Val
+    'L': ['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'], #Leu
+    'T': ['ACU' ,'ACC', 'ACA', 'ACG'], #Thr
+    'K': ['AAA', 'AAG'], #Lys
+    'W': ['UGG'], #Trp
+    'H': ['CAU', 'CAC'], #His
+    'F': ['UUU', 'UUC'], #Phe
+    'I': ['AUU', 'AUC', 'AUA'], #Ile
+    'R': ['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'], #Arg
+    'M': ['AUG'], #Met
+    'A': ['GCU', 'GCC', 'GCA', 'GCG'], #Ala
+    'P': ['CCU', 'CCC', 'CCA', 'CCG'], #Pro
+    'G': ['GGU', 'GGC', 'GGA', 'GGG'], #Gly
+    'S': ['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'], #Ser
+    'C': ['UGU', 'UGC'], #Cys
+    'N': ['AAU', 'AAC'], #Asn
+    'Q': ['CAA', 'CAG'], #Gln
+    'Y': ['UAU', 'UAC'], #Tyr
+    'D': ['GAU', 'GAC'], #Asp
+    'E': ['GAA', 'GAG'], #Glu
 }
 
 def encode(sequence):
-    index_start = 0
-    index_end = index_start + 2
-    while index_end < len(sequence):
-        sequence[index_start, index_end]
+    final_string = ''
+    for char in sequence:
+        possible_codons = genetic_code[char]
+        if(len(possible_codons) > 1):
+            final_code_idx = random.randrange(0, len(possible_codons) - 1)
+            final_string += possible_codons[final_code_idx]
+        else:
+            final_string += possible_codons[0]
+    return final_string
+    
+print(encode(Sec1))
